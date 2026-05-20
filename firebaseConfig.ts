607b2b1782bcafedc6a695965496fa4645a02bc4
kadrayd <109556783+kadrayd@users.noreply.github.com>
2026-05-20 16:00:11 +0300

    firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyCNI8xyiL-ONxRh0dYQiB2LDIq3juD1xD8',
+  authDomain: 'snapora-e2571.firebaseapp.com',
+  projectId: 'snapora-e2571',
+  storageBucket: 'snapora-e2571.firebasestorage.app',
+  messagingSenderId: '294880190972',
+  appId: '1:294880190972:web:96d5f2c0edfbc9b40dd279',
 };
 
 const app = initializeApp(firebaseConfig);
